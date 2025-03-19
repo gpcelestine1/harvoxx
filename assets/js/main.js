286ed0cbb741c5c50eddbbec66a55e0b4933eb4e
Celestine O. Godspower <gpcelestine@gmail.com>
2025-03-19 21:38:30 +0100

    added javascript to control small screen device menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
-
-const contactForm  = document.getElementById("contactForm")
+const contactForm = document.getElementById("contactForm");
 const footerForm = document.getElementById("footerForm");
 contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  validateForm()
+  validateForm();
 });
 
 footerForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    validateForm()
-})
+  e.preventDefault();
+  validateForm();
+});
 
 function validateForm() {
   const name = document.getElementById("name").value;
@@ -22,5 +21,69 @@ function validateForm() {
     return false;
   }
 
-  return true
+  return true;
 }
+
+// const openBtn = document.getElementById("open");
+// const closeBtn = document.getElementById("close");
+
+// openBtn.addEventListener("click", () => {
+//   menu.style.display = "block";
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   menu.style.display = "none";
+// })
+
+// method to control the menu
+// testMenu.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const current = e.currentTarget.classList;
+//     if (current.contains("fa-bars")) {
+//       menu.style.display = "block";
+//     } else {
+//       menu.style.display = "none";
+//     }
+//   });
+// });
+
+// menu variables
+const testMenu = document.querySelectorAll(".menu");
+const menu = document.getElementById("navListMenu");
+const closeIcon = document.getElementById("close");
+const openIcon = document.getElementById("open");
+
+// method to control the menu / open and close icon
+testMenu.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const current = e.currentTarget.classList;
+
+    // menu control structure
+    if (current.contains("fa-bars")) {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+
+    // menu buttion control structure section
+    // to close the open button
+    if (current.contains("fa-bars")) {
+      openIcon.style.display = "none";
+    }
+
+    // removing the close buttion from the initial display of none
+    if (current.contains("fa-bars")) {
+      closeIcon.style.display = "block";
+    }
+
+    // to close the close button
+    if (current.contains("fa-xmark")) {
+      closeIcon.style.display = "none";
+    }
+
+    // to open the open button
+    if (current.contains("fa-xmark")) {
+      openIcon.style.display = "block";
+    }
+  });
+});
